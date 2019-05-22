6f37cea5fd94b88806e9be5ac528c603926343e3
Malavika P Pillai <sreelakamani98@gmail.com>
2019-05-22 23:43:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''
-    <h1>Welcome to the Awesome Fitness Kiosk API Home Page</h1>
-    '''
+@app.route('/')
+def index():
+	return 'OK!'
 if __name__ == "__main__":
 	app.run()
